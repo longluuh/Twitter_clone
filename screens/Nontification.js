29de3f3e78c7a5760fee6face0ba67f1f2cbbdb3
Long Luu <luuhoanglong577@gmail.com>
2022-02-14 08:48:52 +0700

    button newtweet

--- screens/Nontification.js
+++ screens/Nontification.js
@@ -1,9 +1,11 @@
 import React from "react";
 import { View, Text, StyleSheet } from "react-native";
+import NewTweetButton from "../components/NewTweetButton";
 const NontificationScreen = () => {
   return (
     <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
       <Text>Nontification</Text>
+      <NewTweetButton />
     </View>
   );
 };
--- screens/Search.js
+++ screens/Search.js
@@ -1,14 +1,30 @@
 import React from "react";
-import { View, Text, StyleSheet } from "react-native";
+import { View, Text, StyleSheet, TextInput, SafeAreaView } from "react-native";
+import NewTweetButton from "../components/NewTweetButton";
+
 const SearchScreen = () => {
   return (
-    <View style={{ flex: 1, alignItems: "center", justifyContent: "center" }}>
-      <Text>Search</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <TextInput style={styles.input} placeholder="Search"></TextInput>
+      <NewTweetButton />
+    </SafeAreaView>
   );
 };
 export default SearchScreen;
 
 const styles = StyleSheet.create({
-  container: {},
+  container: {
+    flex: 1,
+    alignItems: "center",
+    justifyContent: "flex-start",
+  },
+  input: {
+    width: "50%",
+    height: 40,
+    margin: 22,
+    color: "black",
+    borderWidth: 0.9,
+    borderRadius: 50,
+    textAlign: "center",
+  },
 });
